9921b2f3a204c5477aea1fa0d2d099f66bec2c61
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-09 23:01:58 -0500

    Completes Core Data setup.

---
+++ slapChat/FISMessage.m
@@ -0,0 +1,15 @@
+//
+//  FISMessage.m
+//  slapChat
+//
+//  Created by Ken M. Haggerty on 2/9/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import "FISMessage.h"
+
+@implementation FISMessage
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
